f326be362838aa322f24765badf5fa02c91d7db6
griki4 <95742896+griki4@users.noreply.github.com>
2022-07-11 18:19:20 +0800

    Update 力扣912-排序数组.js

--- 力扣912-排序数组.js
+++ 力扣912-排序数组.js
@@ -19,5 +19,20 @@ var sortArray = function(nums) {
     }
     return sortArray(left).concat(pivotValue,sortArray(right))
 };
-
 //该排序方法会不断开辟新的空间来存储分类后的数组，当对空间复杂度有要求的时候最好让数组进行原地排序（交换顺序）
+
+/*2.冒泡排序
+*   思想是两两比较数组中的数据，前一个数据大于后一个则交换二者的位置
+*   这样遍历一次数组之后数组中最大的数据就被移动到了数组末尾
+*
+* 特点是时间复杂度高但是空间复杂度低*/
+var sortArray = function(nums) {
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = 0; j < i; j++){
+            if (nums[j] > nums[j + 1]) {
+                [nums[j], nums[j+1]] = [nums[j+1], nums[j]]
+            }
+        }
+    }
+    return nums
+};
